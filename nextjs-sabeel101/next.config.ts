39c88de115ffc6bd654b773d14857f0c0635cc4e
Abdullahi Umar Katsayal <aukatsayal001@gmail.com>
2026-01-13 23:17:26 +0100

    netlify functionalities

--- nextjs-sabeel101/next.config.ts
+++ nextjs-sabeel101/next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  typescript: {
+    ignoreBuildErrors: false, 
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
